117c9f029c19b1d610c1a7181b4752cf5ed0b7cf
siobahnschuck <sschuc@saic.edu>
2021-04-08 17:19:11 -0500

    basic structure + basic state

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>NFTy</h1>
+      <p>
+        Simulating a NFT marketplace where posted images become Non-Fungible-Tokens and can be bid on by other users.
+        Currently supports anonymous submissions.
+      </p>
+    </div>
+  )
+}
+
+export default Header
