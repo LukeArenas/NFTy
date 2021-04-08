fa6c5fe2ce43d681c915eb8565c009d12b146864
queeniewilliams <queeniepu329@gmail.com>
2021-04-08 14:31:44 -0700

    back end completed

---
+++ models/post.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from models.db import db
+
+class Post(db.Model):
+  __tablename__='posts'
+
+  id=db.Column(db.Integer, primary_key=True)
+  username=db.Column(db.String(255), nullable=False)
+  image=db.Column(db.String(255), nullable=False)
+  description=db.Column(db.Text, nullable=False)
+  bid=db.Column(db.Float, nullable=False)
+  created_at = db.Column(db.DateTime, default=str(datetime.utcnow()), nullable=False)
+  updated_at = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False, onupdate=datetime.now())
+
+  def __init__(self,username,image,bid,description):
+    self.username=username
+    self.image=image
+    self.bid=bid
+    self.description=description
+
+  def json(self):
+    return{"id":self.id,"image":self.image,"bid":self.bid,"description":self.description,"created_at":str(self.created_at),"updated_at":str(self.updated_at)}
+
+  def create(self):
+    db.session.add(self)
+    db.session.commit()
+    return self
+
+  @classmethod
+  def find_all(cls):
+    return Post.query.all()
+
+  @classmethod
+  def find_by_id(cls,post_id):
+    post=Post.query.filter_by(id=post_id).first()
+    return post
---
+++ resources/post.py
@@ -0,0 +1,34 @@
+from flask import request
+from flask_restful import Resource
+from models.post import Post
+from models.db import db
+
+
+
+class Posts(Resource):
+  def get(self):
+    posts=Post.find_all()
+    return [data.json()for data in posts]
+
+  def post(self):
+    data=request.get_json()
+    post=Post(**data)
+    post.create()
+    return post.json(),201
+
+class PostDetail(Resource):
+  def put(self,post_id):
+    data=request.get_json()
+    post=Post.find_by_id(post_id)
+    for key in data:
+      setattr(post,key,data[key])
+      db.session.commit()
+      return post.json()
+
+  def delete(self,post_id):
+    post=Post.find_by_id(post_id)
+    if not post:
+      return {"msg":"Post not found"},404
+    db.session.delete(post)
+    db.session.commit()
+    return{"msg":"Post Deleted","payload":post_id}
