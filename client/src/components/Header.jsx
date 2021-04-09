ade78af1b4ab7f9b779471b941abd8f4edf624f2
queeniewilliams <queeniepu329@gmail.com>
2021-04-09 07:08:53 -0700

    editing logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-      <h1 style={{ color: 'rgb(241, 229, 54)' }}>NFTy</h1>
+      <h1 className="logo">NFTy</h1>
       <p>
         Simulating a NFT marketplace where posted images become
         Non-Fungible-Tokens and can be bid on by other users. Currently supports
